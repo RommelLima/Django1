dbda4cc6920c0d1377a147047ae1811b8d970298
Rommel Wladimir de Lima <rommelwladimir@uern.br>
2021-02-03 15:12:10 -0300

    Aula sobre sistema de templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_blog(request):
+    lista = ['Django', 'Python', 'Git', 'Html',
+             'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
+             'Systemctl']
+    data = {'name': 'Curso de Django', 'list_tecnology': lista}
+    return render(request, 'index.html', data)
