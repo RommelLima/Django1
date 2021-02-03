519ba5a08c2811e099a061683af2918ebed2bcd6
Rommel Wladimir de Lima <rommelwladimir@uern.br>
2021-02-03 20:09:22 -0300

    Seção 2 concluída

--- blog/website/models.py
+++ blog/website/models.py
@@ -2,7 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Categories(models.TextChoices):
+    TECH = 'TC', 'Tecnologia'
+    CR = 'CR', 'Curiosidades'
+    GR = 'GR', 'Geral'
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Categories.choices,
+        default=Categories.GR,
+    )
+    deleted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+    def full_name(self):
+        return self.title + self.sub_title
+
+    full_name.admin_order_field = 'title'
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html',
              'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
              'Systemctl']
-    data = {'name': 'Curso de Django', 'list_tecnology': lista}
+
+    list_post = Post.objects.all
+    data = {'name': 'Curso de Django',
+            'list_tecnology': lista,
+            'posts': list_post }
+
+
+
     return render(request, 'index.html', data)
